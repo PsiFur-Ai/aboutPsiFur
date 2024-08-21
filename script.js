6d04300242437496433abb6444214906bf0cf842
Harshit Tiwari <tech.harshit.tiwari@gmail.com>
2024-08-21 20:24:03 +0530

    added stuff

---
+++ script.js
@@ -0,0 +1,4 @@
+// Example script to handle the Try Out Now button click
+document.querySelector('.try-now').addEventListener('click', function() {
+    alert('PsiFur AI is coming soon! Stay tuned.');
+});
